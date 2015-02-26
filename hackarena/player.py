6576e8d010ad164253cb1b673ee60c6fbcaf25aa
Joe Bateson <joe@joebateson.com>
2015-02-26 19:23:01 +0100

    Add check for available spells

--- hackarena/player.py
+++ hackarena/player.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 import hackarena.constants
+from hackarena.constants import Classes
+from hackarena.constants import Spell
 from hackarena.game_objects import BaseGameObject
+
+
+AVAILABLE_SPELLS = {
+    Classes.TANK: [Spell.TANK_ATTACK, Spell.TANK_AOE],
+    Classes.MAGE: [Spell.MAGE_DIRECT_DAMAGE, Spell.MAGE_AOE],
+    Classes.HEALER: [Spell.HEALER_DIRECT_DAMAGE, Spell.HEALER_HEAL]
+}
 
 
 class Player(BaseGameObject):
@@ -15,6 +24,7 @@ class Player(BaseGameObject):
     ):
         self.username = username
         self.character_class = character_class
+        self.available_spells = AVAILABLE_SPELLS[character_class]
         self.team = team
         self.position = {
             'x': 20 if team == 'blue' else hackarena.constants.map_width - 20,
