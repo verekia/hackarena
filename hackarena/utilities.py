47530321c413976241e0d4e314f2a8e1532f38c9
Joe Bateson <joe@joebateson.com>
2015-02-26 12:53:51 +0100

    Fix classmethods on utility class

--- hackarena/utilities.py
+++ hackarena/utilities.py
@@ -2,13 +2,12 @@
 
 
 class Utilities(object):
-    def get_session_string(self, original_session_string):
+
+    @classmethod
+    def get_session_string(cls, original_session_string):
         session_attributes = original_session_string.split(' ')
         return session_attributes[0] + ' ' + session_attributes[1]
 
-    def get_session_middle_part(self, original_session_string):
+    @classmethod
+    def get_session_middle_part(cls, original_session_string):
         return original_session_string.split(' ')[1]
-
-    def generate_random_name(self, original_session_string):
-        # Should get improved
-        return self.get_session_middle_part(original_session_string)
