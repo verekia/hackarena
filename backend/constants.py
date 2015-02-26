61f2c33c114a82e880ef9b6981d214b1ed99c228
Joe Bateson <joe@joebateson.com>
2015-02-26 10:59:08 +0100

    Set up initial backend structure

---
+++ backend/constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Spell(Enum):
+    MAGE_FIREBALL = 'MAGE_FIREBALL'
+    WARRIOR_ATTACK = 'WARRIOR_ATTACK'
---
+++ backend/messages.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class BEMessages(Enum):
+    ALL_MAIN_BROADCAST = 'ALL_MAIN_BROADCAST'
+
+
+class FEMessages(Enum):
+    pass
+
+
+class AllMainBroadCast(object):
+
+    def __init__(self):
+        pass
+
+    def broadcast(self):
+        pass
